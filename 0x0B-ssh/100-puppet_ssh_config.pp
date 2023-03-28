483d13ef0055f2fd55cbba48180e445c1dc08073
straystringz <victor.j.emmanuel@hotmail.com>
2023-03-28 04:02:14 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -11,6 +11,6 @@ file_line { 'Turn off passwd auth':
 file_line { 'Declare identity file':
   ensure  => present,
   path    => '/etc/ssh/ssh_config',
-  line    => '    IdentityFile ~/.ssh.school',
+  line    => '    IdentityFile root/.ssh.school',
   replace => true,
 }
