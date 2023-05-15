a3dc46ca7debaae05741501bd39a836187bf321a
Victor Emmanuel <victor.j.emmanuel@hotmail.com>
2023-05-15 16:02:30 +0000

    Strace is your friend

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixing the php error
-     $php_rout = '/var/www/html/wp-settings.php'
+  $php_rout = '/var/www/html/wp-settings.php'
 
     exec { 'replace_line':
       command => "sed -i 's/phpp/php/g' ${php_rout}",
